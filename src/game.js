e8ca8738378d372b53f4357a2bb55a809420d51a
Robert <roberts.tohver@pipedrive.com>
2020-05-13 19:21:06 +0300

    master : Updates

--- src/game.js
+++ src/game.js
@@ -1,4 +1,4 @@
 import Vue from 'vue';
-import Game from './Game.vue';
+import Game from './pages/host/Game.vue';
 import 'bulma';
 new Vue({ render: createElement => createElement(Game) }).$mount('#app');
