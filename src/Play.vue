616b4cb0aefdfbb3498b83ffb5fc2309703231b1
Jasper noorkõiv <jasper.noorkoiv@tptlive.ee>
2020-06-16 01:10:25 +0300

    Add dark theme

--- src/Play.vue
+++ src/Play.vue
@@ -1,12 +1,13 @@
 <template>
-    <div class="container">
+    <div>
+        <button v-on:click="switchDarkTheme()">{{ darkThemeText }}</button>
         <div v-if="!joined">
             <input class="input" type="text" placeholder="code" v-model="code">
             <input class="input" type="text" placeholder="name" v-model="name">
             <button class="button" @click="join">Join</button>
         </div>
         <div v-if="started">
-            Game Has Started
+            <h1>Game Has Started</h1>
             <h1 v-if="timer">{{timer}}</h1>
             <div class="columns" v-if="options.length">
                 <div class="column is-one-quarter" v-for="option in options">
@@ -47,7 +48,9 @@
                 uid: null,
                 started: false,
                 options: [],
-                timer: 0
+                timer: 0,
+                darkThemeText: 'Dark theme',
+                isDarkTheme: false
             }
         },
         methods: {
@@ -64,11 +67,23 @@
             answer(option){
                 this.ws.send(JSON.stringify({sender:'player', id: this.uid, action:'answer', data: {answer: option}}));
                 this.options = [];
+            },
+            switchDarkTheme() {
+                const body = document.body;
+                this.isDarkTheme = !this.isDarkTheme
+
+                if (this.isDarkTheme) {
+                    body.classList.add('dark-theme');
+                } else {
+                    body.classList.remove('dark-theme');
+                }
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .dark-theme.container {
+        background-color: #000;
+    }
 </style>
